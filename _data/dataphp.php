12c71dfa5acd556fd338f6d5959b3e43d131991c
Ashish Maheshwari <53733415+AshishMaheshwari5959@users.noreply.github.com>
2020-05-02 23:33:23 +0530

    Delete dataphp.php

--- _data/dataphp.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//phpinfo();
-	system("php database.php");
-	echo "Registration Sucessfully...\nRedirecting to Login Page in 2 seconds....";
-    header("refresh:2; url=Homepage.html");
-?>
-
